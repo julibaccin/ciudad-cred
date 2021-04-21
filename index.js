db9f604733abad71455f27b84010ad3d970f35db
Julian Baccin <julibaccin01@gmail.com>
2021-04-21 08:24:23 -0300

    frm

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ form.addEventListener('submit', (e)=>{
 
 e.preventDefault()
 console.log(nombre.value);
-let cadena = `mailto:consultar@ciudadcred.com?Subject=Desde%20Pagina%20web&body=Nombre%20${nombre.value}
-              %20DNI%20${dni.value}
-              %20Telefono%20${telefono.value}
-              %20Email:%20${email.value}
-              %20Genero%20${genero.value}
-              %20Cuanto:%20${cuanto.value}
+let cadena = `mailto:consultar@ciudadcred.com?Subject=Desde%20Pagina%20web&body=Nombre:%20${nombre.value}%0A
+              %20DNI:%20${dni.value}%0A
+              %20Telefono:%20${telefono.value}%0A
+              %20Email:%20${email.value}%0A
+              %20Genero:%20${genero.value}%0A
+              %20Cuanto:%20${cuanto.value}%0A
               %20Cuotas:%20${cuotas.value}`
 
 window.open(cadena);        
