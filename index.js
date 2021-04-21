a71ae99811728e965e2ad3083a135629a06a77a5
Julian Baccin <julibaccin01@gmail.com>
2021-04-21 08:28:31 -0300

    frm

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const form = document.getElementById('form');
 const nombre = document.getElementById('nombre')
 const dni = document.getElementById('dni')
 const telefono = document.getElementById('telefono')
-const email = document.getElementById('email')
 const genero = document.getElementById('genero')
 const cuanto = document.getElementById('cuanto')
 const cuotas = document.getElementById('cuotas')
@@ -11,13 +10,13 @@ form.addEventListener('submit', (e)=>{
 
 e.preventDefault()
 console.log(nombre.value);
-let cadena = `mailto:consultar@ciudadcred.com?Subject=Desde%20Pagina%20web&body=Nombre:%20${nombre.value}%0A
-              %20DNI:%20${dni.value}%0A
-              %20Telefono:%20${telefono.value}%0A
-              %20Email:%20${email.value}%0A
-              %20Genero:%20${genero.value}%0A
-              %20Cuanto:%20${cuanto.value}%0A
-              %20Cuotas:%20${cuotas.value}`
+let cadena = `mailto:consultar@ciudadcred.com?Subject=Desde%20Pagina%20web&body=
+              Nombre:%20${nombre.value}%0A
+              DNI:%20${dni.value}%0A
+              Telefono:%20${telefono.value}%0A
+              Genero:%20${genero.value}%0A
+              Cuanto:%20${cuanto.value}%0A
+              Cuotas:%20${cuotas.value}`
 
 window.open(cadena);        
 
