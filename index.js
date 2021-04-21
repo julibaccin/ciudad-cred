5307ad1f2efd4e5a00976271f93ab5f557c00fde
Julian Baccin <julibaccin01@gmail.com>
2021-04-21 08:38:13 -0300

    frm

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ const form = document.getElementById('form');
 const nombre = document.getElementById('nombre')
 const dni = document.getElementById('dni')
 const telefono = document.getElementById('telefono')
-const genero = document.getElementById('genero')
 const cuanto = document.getElementById('cuanto')
 const cuotas = document.getElementById('cuotas')
 
 form.addEventListener('submit', (e)=>{
 
 e.preventDefault()
-console.log(nombre.value);
+
 let cadena = `mailto:consultas@ciudadcred.com?Subject=Desde%20Pagina%20web&body=
               Nombre:%20${nombre.value}%0A
               DNI:%20${dni.value}%0A
               Telefono:%20${telefono.value}%0A
-              Genero:%20${genero.value}%0A
               Cuanto:%20${cuanto.value}%0A
               Cuotas:%20${cuotas.value}`
 
