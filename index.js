aa51c9fdf43b71becf6440c3d41289ec5f738779
Julian Baccin <julibaccin01@gmail.com>
2021-04-23 09:59:45 -0300

    asd

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('form');
+const myModal = new bootstrap.Modal(document.getElementById('exampleModal'), options)
 const nombre = document.getElementById('nombre')
 const dni = document.getElementById('dni')
 const telefono = document.getElementById('telefono')
@@ -15,6 +16,8 @@ let cadena = `mailto:consultas@ciudadcred.com?Subject=Desde%20Pagina%20web&body=
               Telefono:%20${telefono.value}%0A
               Cuanto:%20${cuanto.value}%0A
               Cuotas:%20${cuotas.value}`
+
+myModal.hide();
 
 window.open(cadena,'_self')
 
